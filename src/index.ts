c5d95a58f1f46dd556742926dc15e5b12e66e212
Damian Bushong <141002651+damianb@users.noreply.github.com>
2018-10-31 15:02:37 -0500

    Export the interfaces via the index file as well.

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,10 @@
 // @license MIT license
 //
 
+export { DepNode } from './interfaces/DepNode'
 export { DepResolver } from './DepResolver'
+export { DepResolverResult } from './interfaces/DepResolverResult'
+export { NodeMap } from './interfaces/NodeMap'
+export { TreeNode } from './interfaces/TreeNode'
 export { TreeResolver } from './TreeResolver'
+export { TreeResolverResult } from './interfaces/TreeResolverResult'
